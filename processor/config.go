3077d7436ef63a3f797fe68755eeb3af337a3176
jarlaw21304 <jarlaw@gmail.com>
2025-05-15 20:24:50 -0700

    Update config.go
    update with import

--- processor/config.go
+++ processor/config.go
@@ -1,8 +1,11 @@
 package processor
 
+import "go.opentelemetry.io/collector/component"
+
 type Config struct {
-    Pattern      string            `mapstructure:"pattern"`
-    VendorFormat string            `mapstructure:"vendor_format"`
-    FieldMap     map[string]string `mapstructure:"field_map"`
-    ExtractCEF   bool              `mapstructure:"extract_cef"`
+    component.Config
+    Pattern          string            `mapstructure:"pattern"`             // Grok parsing pattern
+    PatternDirectory string            `mapstructure:"pattern_directory"`   // Directory where .grok pattern files are stored
+    FieldMap         map[string]string `mapstructure:"field_map"`           // Field mapping
+    ExtractCEF       bool              `mapstructure:"extract_cef"`
 }
