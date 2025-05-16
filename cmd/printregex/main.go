a69fa50e564bc8cb9c24374dcc0984716ce46821
jarlaw21304 <jarlaw@gmail.com>
2025-05-16 13:44:07 -0700

    Update main.go

--- cmd/printregex/main.go
+++ cmd/printregex/main.go
@@ -1,32 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"github.com/jarlaw21304/otel-grokproc/processor/grokparse"
-	"os"
+    "fmt"
+    "os"
+    "github.com/jarlaw21304/otel-grokproc/processor/grokparse"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: printregex <pattern-dir> <grok-pattern>")
-		os.Exit(1)
-	}
-	patternDir := os.Args[1]
-	pattern := os.Args[2]
+    if len(os.Args) < 3 {
+        fmt.Fprintln(os.Stderr, "Usage: printregex <pattern-dir> <grok-pattern> [not-match-at-start]")
+        os.Exit(1)
+    }
+    patternDir := os.Args[1]
+    pattern := os.Args[2]
 
-	err := grokparse.LoadAllPatternFiles(patternDir)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error loading pattern files: %v\n", err)
-		os.Exit(1)
-	}
-	expanded, err := grokparse.GetExpandedRegex(pattern)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error expanding pattern: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(expanded)
+    // Optional: third argument for "not match" prefix
+    notMatchPrefix := ""
+    if len(os.Args) > 3 {
+        notMatch := os.Args[3]
+        if notMatch != "" {
+            notMatchPrefix = "^(?!" + notMatch + ")"
+        }
+    }
 
-	// Wrap with negative lookahead or preamble here
-	notMatchPrefix := "^(?!BAD)"
-	fmt.Println(notMatchPrefix + expanded)
+    err := grokparse.LoadAllPatternFiles(patternDir)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error loading pattern files: %v\n", err)
+        os.Exit(1)
+    }
+
+    expanded, err := grokparse.GetExpandedRegex(pattern)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error expanding pattern: %v\n", err)
+        os.Exit(1)
+    }
+
+    fmt.Println(notMatchPrefix + expanded)
 }
+
